cfab196d64d76cdb6b26fa5184e19496aedb25a7
prasad145 <acharyaprasad145@gmail.com>
2021-06-07 16:18:30 +0530

    java practice

--- Basics/objectCreationNew.java
+++
@@ -1,9 +0,0 @@
-
-public class objectCreationNew {
-	int a = 10;
-	public static void main(String[] args)
-	{
-		objectCreationNew ocn = new objectCreationNew();
-		System.out.println(ocn.a);
-	}
-}
